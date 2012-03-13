c7f421c07d47aaa2a6fd586b046245ad04c42a57
Curtis Schofield <curtis@ram9.cc>
2012-03-13 12:32:27 -0700

    rename index to blog

--- blog.rb
+++ blog.rb
@@ -10,6 +10,6 @@ class Blog < Sinatra::Base
     HTTParty.get('http://blazingcloud.net/feed/json').each do |article|
       articles << erb(:article, :locals => {:article => OpenStruct.new(article)},:layout => false)
     end
-    erb(:index, :locals => {:articles => articles.string})
+    erb(:blog, :locals => {:articles => articles.string})
   end
 end
