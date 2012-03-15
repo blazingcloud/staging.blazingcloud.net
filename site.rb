5c324abc339900c6fa138a22024c74ed5c88d7c5
Curtis Schofield <curtis@ram9.cc>
2012-03-15 15:38:33 -0700

    section titles, homepage intro, blog single column, column tweaks, seperate 'main' into 'tile' for articles

--- site.rb
+++ site.rb
@@ -2,6 +2,12 @@
 require 'sinatra/base'
 class Site < Sinatra::Base
   get '/' do
+    erb(:homepage)
+  end
+  get '/service' do
+    erb(:service)
+  end
+  get '/services' do
     erb(:service)
   end
 end
