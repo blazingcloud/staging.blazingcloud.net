85fe5e05769e8f9eb5399a2b7f309bdb458adebf
Curtis Schofield <curtis@ram9.cc>
2012-05-02 11:51:59 -0700

    cleanup

--- incoming.rb
+++ incoming.rb
@@ -10,38 +10,21 @@ class Incoming < Sinatra::Base
     auth_token  = ENV['DEPLOY_TWILIO_AUTHTOKEN']
     from_phone  = '14157994519' # JUDY's TWILO Internal Number 
     @client ||= Twilio::REST::Client.new(account_sid,auth_token)
-      puts @client
-    puts "9" * 70
-    # grab an account instance resource if you know the sid
-    @account = @client.accounts.get(account_sid)
-    # http round trip happens here
-    puts @account.friendly_name
-
-    puts "9" * 70
-
-    puts "1" * 70
     to_numbers.each do |to_number|
-    puts "2" * 70
-      puts @client.account.sms.messages.create(
+      @client.account.sms.messages.create(
         :from => from_phone,
         :to => to_number,
         :body => "[Depot][Incoming::Deploy] #{things.join(' | ')}"
       )
     end
-    puts "3" * 70
-
   end
 
   post '/deploy/:service' do
-    puts "0" * 70
-
     param_string = params.inject("") do |memo,kv|
       memo += "<div><span>#{kv.first}</span> => <span>#{kv.last}</span></div>"
     end
 
-    puts 'depot'
     send_deploy_messages(params[:app], params[:url])
-    puts params
     "Incoming :: /deploy/#{params[:service]} :: #{param_string }"
   end
 end
